13d0e5310e135c0d65ac7c5417895cab676fe810
josfam <josephamukun@tutanota.com>
2024-03-31 12:59:11 +0300

    Add linux fresh install script

---
+++ fresh-linux-install.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+
+######################### UTILS #########################
+
+sudo apt install -y git \
+		 curl \
+
+
+######################### DEVELOPMENT TOOLS #########################
+
+sudo apt install -y build-essential
+sudo apt cmake
+
+### dependencies for building python from source later
+sudo apt install -y \
+		 pkg-config \
+		 python3-tk \
+		 libsqlite3-dev \
+		 libncurses5-dev \
+		 libgdbm-dev \
+		 libbz2-dev \
+		 libdb-dev \
+		 uuid-dev \
+		 libffi-dev \
+		 liblzma-dev \
+		 libreadline-dev \
+		 libssl-dev \
+		 tk-dev \
+		 zlib1g-dev
+
+
+######################### ZSH AND OH-MY-ZSH #########################
+
+### zsh
+sudo apt install zsh
+
+### oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+
+######################### BROWSERS #########################
+
+### Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install brave-browser
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+
+######################### MESSAGING #########################
+
+### Signal Desktop
+
+# NOTE: These instructions only work for 64-bit Debian-based
+# Linux distributions such as Ubuntu, Mint etc.
+
+# 1. Install our official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
